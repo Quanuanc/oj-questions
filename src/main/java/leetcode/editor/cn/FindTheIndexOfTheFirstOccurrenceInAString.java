2994cb2e11ee3f047524145284691e506066d0a2
Quanuanc <quanuanc@outlook.com>
2022-11-11 00:35:30 +0800

    feat: add q28

--- src/main/java/leetcode/editor/cn/FindTheIndexOfTheFirstOccurrenceInAString.java
+++ src/main/java/leetcode/editor/cn/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -1,13 +1,20 @@
 package leetcode.editor.cn;
 
+/**
+ * [28] 找出字符串中第一个匹配项的下标
+ */
 public class FindTheIndexOfTheFirstOccurrenceInAString {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int strStr(String haystack, String needle) {
-            //TODO
-            return 0;
+            return haystack.indexOf(needle);
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
+    public static void main(String[] args) {
+        String hay = "leetcode", need = "leetco";
+        int result = new FindTheIndexOfTheFirstOccurrenceInAString().new Solution().strStr(hay, need);
+        System.out.println(result);
+    }
 }
