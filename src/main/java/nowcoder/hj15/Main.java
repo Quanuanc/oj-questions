ceeefa71dc99f35670212c5f9d02f64997d1acf6
全国成 <quanuanc@outlook.com>
2023-04-23 10:23:29 +0800

    style: Reformat code

--- src/main/java/nowcoder/hj15/Main.java
+++ src/main/java/nowcoder/hj15/Main.java
@@ -9,7 +9,7 @@ public class Main {
         String binaryString = Integer.toBinaryString(i);
         int count = 0;
         for (int i1 = 0; i1 < binaryString.length(); i1++) {
-            if(binaryString.charAt(i1) == '1'){
+            if (binaryString.charAt(i1) == '1') {
                 count++;
             }
         }
