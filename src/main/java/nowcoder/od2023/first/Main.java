6ce8753b368375c6399343744aef18877c410d66
Quanuanc <quanuanc@outlook.com>
2023-05-05 21:30:30 +0800

    Add od1 comments

--- src/main/java/nowcoder/od2023/first/Main.java
+++ src/main/java/nowcoder/od2023/first/Main.java
@@ -12,11 +12,15 @@ public class Main {
         List<Integer> nums = Arrays.stream(in.nextLine().split(" "))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
-        Map<Integer, Integer> countMap = new HashMap<>();
 
+        // 1. 统计每个牌号的数量
+        // k -> v : 牌号 -> 牌数
+        Map<Integer, Integer> countMap = new HashMap<>();
         for (Integer num : nums) {
             countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
+
+        // 2. 按照 牌数 > 牌号 的规则排序
         List<Map.Entry<Integer, Integer>> countList = new ArrayList<>(countMap.entrySet());
         countList.sort((a, b) -> {
             if (Objects.equals(b.getValue(), a.getValue())) {
@@ -24,17 +28,20 @@ public class Main {
             }
             return b.getValue() - a.getValue();
         });
+
+        // 3. 遍历 countList，按照 炸弹 > 葫芦 > 三张 > 对子 > 单张 的规则来生成字符串
         StringBuilder result = new StringBuilder();
-        List<Integer> pukeCards = new ArrayList<>();
+        List<Integer> pukeCards = new ArrayList<>(); //暂存由于凑葫芦而剩余的牌
         for (int i = 0; i < countList.size(); i++) {
             Map.Entry<Integer, Integer> entry = countList.get(i);
-            int cardNum = entry.getKey();
-            int cardCount = entry.getValue();
-            if (i > 0 && countList.get(i - 1).getValue() == 3 && cardCount == 3) {
+            int cardNum = entry.getKey(); //牌号
+            int cardCount = entry.getValue(); //牌数
+
+            if (i > 0 && countList.get(i - 1).getValue() == 3 && cardCount == 3) { //上一轮已经凑到了三张，这一轮也有三张，那就把这一轮的三张抽两张出来凑成葫芦
                 pukeCards.add(cardNum);
                 cardCount--;
                 entry.setValue(cardCount);
-            } else if (cardCount == 1 && !pukeCards.isEmpty()) {
+            } else if (cardCount == 1 && !pukeCards.isEmpty()) { //如果本轮的牌为单张，就需要和暂存的牌先比较大小，大的先输出
                 for (int k = 0; k < pukeCards.size(); k++) {
                     if (pukeCards.get(k) > cardNum) {
                         result.append(pukeCards.get(k)).append(" ");
