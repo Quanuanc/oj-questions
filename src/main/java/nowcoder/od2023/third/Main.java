36ad5dcf6bd5a6a31c79a43af3c135a1655f066f
全国成 <quanuanc@outlook.com>
2023-05-06 15:00:10 +0800

    feat(problem): q118

--- src/main/java/nowcoder/od2023/third/Main.java
+++ src/main/java/nowcoder/od2023/third/Main.java
@@ -11,23 +11,23 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int myMoney = Integer.parseInt(input.nextLine());
+        int money = Integer.parseInt(input.nextLine());
         List<Integer> messages = Arrays.stream(input.nextLine().split(" "))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
 
-        int[] dp = new int[myMoney + 1];
-        for (int i = 0; i <= messages.size(); i++) {
-            for (int j = 0; j <= myMoney; j++) {
-                if (i == 0 || j == 0 || j < i) {
+        int[] dp = new int[money + 1];
+
+        for (int i = 0; i <= messages.size(); i++) { // i->短信价格，j->手头的钱
+            for (int j = 0; j <= money; j++) {
+                if (i == 0 || j == 0 || j < i)
                     continue;
-                }
-                int A = dp[j];
-                int B = dp[j - i] + messages.get(i - 1);
-                dp[j] = Math.max(A, B);
+                int notSel = dp[j]; // 不选 手头钱的最优解
+                int sel = dp[j - i] + messages.get(i - 1); // 选 dp[j - i] 手头钱-当前短信价格的最优解，message(i - 1)当前短信的条数
+                dp[j] = Math.max(sel, notSel);
             }
         }
 
-        System.out.println(dp[myMoney]);
+        System.out.println(dp[money]);
     }
 }
