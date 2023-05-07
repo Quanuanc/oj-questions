79204e65c9463b510f2e60a9735945e1cdfa5cb4
Quanuanc <quanuanc@outlook.com>
2023-05-07 17:13:15 +0800

    Add solution

---
+++ src/main/java/leetcode/editor/cn/FindTheDifference.java
@@ -0,0 +1,27 @@
+package leetcode.editor.cn;
+
+/**
+ * [389] 找不同
+ */
+public class FindTheDifference {
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public char findTheDifference(String s, String t) {
+            int[] charCount = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                charCount[c - 'a']++;
+            }
+            for (int i = 0; i < t.length(); i++) {
+                char c = t.charAt(i);
+                charCount[c - 'a']--;
+                if (charCount[c - 'a'] < 0) {
+                    return c;
+                }
+            }
+            return 'A';
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
