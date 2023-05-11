3c75d35709f6e8cf33486ed59c533abc55660bca
Quanuanc <quanuanc@outlook.com>
2023-05-11 22:46:54 +0800

    review q704

--- src/main/java/leetcode/editor/cn/BinarySearch.java
+++ src/main/java/leetcode/editor/cn/BinarySearch.java
@@ -7,17 +7,30 @@ public class BinarySearch {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int search(int[] nums, int target) {
-            int left = 0, right = nums.length - 1;
-            int mid = left + (right - left) / 2;
+            int left = 0, right = nums.length - 1; //左右都在num区间范围内
             while (left <= right) {
-                if (nums[mid] > target) {
-                    right = mid - 1;
-                    mid = left + (right - left) / 2;
-                } else if (nums[mid] < target) {
-                    left = mid + 1;
-                    mid = left + (right - left) / 2;
+                int middle = left + (right - left) / 2;
+                if (target > nums[middle]) {
+                    left = middle + 1;
+                } else if (target < nums[middle]) {
+                    right = middle - 1;
                 } else {
-                    return mid;
+                    return middle;
+                }
+            }
+            return -1;
+        }
+
+        public int search2(int[] nums, int target) {
+            int left = 0, right = nums.length; //左在，右不在
+            while (left < right) {
+                int middle = left + (right - left) / 2;
+                if (target > nums[middle]) {
+                    left = middle + 1;
+                } else if (target < nums[middle]) {
+                    right = middle;
+                } else {
+                    return middle;
                 }
             }
             return -1;
